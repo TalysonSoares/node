dc8979eabd1011d99492e82bfa816cc4057087d0
TalysonSoares <talysonsoares40@gmail.com>
2022-10-17 11:23:39 -0300

    first commit

---
+++ arquivo.js
@@ -0,0 +1,32 @@
+let fs = require('fs');
+
+fs.mkdir('exercicio01', () => {});
+
+let hoje = new Date();
+
+let ano = hoje.getFullYear();
+let mes = hoje.getMonth() + 1;
+let dia = hoje.getDate();
+
+
+
+fs.mkdir('exercicio01/'+ano, () => {
+    fs.mkdir(`exercicio01/${ano}/${mes}`, () => {
+        fs.mkdir(`exercicio01/${ano}/${mes}/${dia}`, () => {
+
+            let alunos = [
+                'Maria',
+                'Chiquim',
+                'Zezim',
+                'Tiao',
+            ];
+            
+            alunos.map((cadaAluno) => {
+                fs.appendFile(`exercicio01/${ano}/${mes}/${dia}/alunos.txt`, cadaAluno+'\n', () => {})
+            })
+            
+        });
+    });
+});
+
+
---
+++ teste.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        name: 'ale',
+        email: 'ale@email.com',
+        city: 'FortalCity'
+    },
+    {
+        name: 'talyson',
+        email: 'talyson@email.com',
+        city: 'FortalCity'
+    },
+    {
+        name: 'xeroverde',
+        email: 'xero@email.com',
+        city: 'FortalCity'
+    }
+];
+
+console.table(dados);
